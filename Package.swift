c20c8835f14770539f019e02fd364cb868bae0d0
Jakub Olejník <olejnjak@gmail.com>
2019-09-08 17:55:08 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,16 +1,18 @@
 // swift-tools-version:5.0
 import PackageDescription
 
-let pkg = Package(name: "WindowTransitions")
-pkg.platforms = [
-    .iOS(.v9)
-]
-pkg.products = [
-    .library(name: "WindowTransitions", targets: ["WindowTransitions"])
-]
-
-let pmk: Target = .target(name: "WindowTransitions")
-pmk.path = "WindowTransitions"
-pkg.targets = [
-    pmk
-]
+let package = Package(
+    name: "WindowTransitions",
+    platforms: [
+        .iOS(.v9),
+    ],
+    products: [
+        .library(name: "WindowTransitions", targets: ["WindowTransitions"]),
+    ],
+    targets: [
+        .target(name: "WindowTransitions", path: "WindowTransitions")
+    ],
+    swiftLanguageVersions: [
+        .v5
+    ]
+)
